596b156469dbcbdb66721880049ccb101c4f7dde
Zilin Song <harry.song518@gmail.com>
2025-04-04 23:59:05 +1030

    remove cardfooter

--- portfolio/src/app/components/JobCard.tsx
+++ portfolio/src/app/components/JobCard.tsx
@@ -1,62 +1,75 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function JobCard() {
   return (
     <div className='flex flex-col gap-4'>
       <Card className='shadow-none'>
         <CardHeader>
-        <div className='flex flex-row items-center justify-between'>
-          <CardTitle className='text-lg font-mono'>Research Assistant</CardTitle>
-          <CardDescription className='text-lg font-mono'>12.2024-06.2024</CardDescription>
-        </div>
+          <div className='flex flex-row items-center justify-between'>
+            <CardTitle className='text-lg font-mono'>
+              Research Assistant
+            </CardTitle>
+            <CardDescription className='text-lg font-mono'>
+              12.2024-06.2024
+            </CardDescription>
+          </div>
         </CardHeader>
         <CardContent>
-        <p className='text-lg font-mono'>AIML</p>
+          <p className='text-lg font-mono'>AIML</p>
         </CardContent>
       </Card>
 
       <Card className='shadow-none'>
         <CardHeader>
-        <div className='flex flex-row items-center justify-between'>
-          <CardTitle className='text-lg font-mono'>Front-end Developer</CardTitle>
-          <CardDescription className='text-lg font-mono'>06.2024-11.2024</CardDescription>
-        </div>
+          <div className='flex flex-row items-center justify-between'>
+            <CardTitle className='text-lg font-mono'>
+              Front-end Developer
+            </CardTitle>
+            <CardDescription className='text-lg font-mono'>
+              06.2024-11.2024
+            </CardDescription>
+          </div>
         </CardHeader>
         <CardContent>
-        <p className='text-lg font-mono'>Luga AI</p>
+          <p className='text-lg font-mono'>Luga AI</p>
         </CardContent>
       </Card>
 
       <Card className='shadow-none'>
         <CardHeader>
-        <div className='flex flex-row items-center justify-between'>
-          <CardTitle className='text-lg font-mono'>Front-end Developer</CardTitle>
-          <CardDescription className='text-lg font-mono'>11.2023-05.2024</CardDescription>
-        </div>
+          <div className='flex flex-row items-center justify-between'>
+            <CardTitle className='text-lg font-mono'>
+              Front-end Developer
+            </CardTitle>
+            <CardDescription className='text-lg font-mono'>
+              11.2023-05.2024
+            </CardDescription>
+          </div>
         </CardHeader>
         <CardContent>
-        <p className='text-lg font-mono'>Rhizo Pty Ltd</p>
+          <p className='text-lg font-mono'>Rhizo Pty Ltd</p>
         </CardContent>
       </Card>
 
       <Card className='shadow-none'>
         <CardHeader>
-        <div className='flex flex-row items-center justify-between'>
-          <CardTitle className='text-lg font-mono'>SAP Consultant</CardTitle>
-          <CardDescription className='text-lg font-mono'>08.2020-01.2023</CardDescription>
-        </div>
+          <div className='flex flex-row items-center justify-between'>
+            <CardTitle className='text-lg font-mono'>SAP Consultant</CardTitle>
+            <CardDescription className='text-lg font-mono'>
+              08.2020-01.2023
+            </CardDescription>
+          </div>
         </CardHeader>
         <CardContent>
-        <p className='text-lg font-mono'>Deloitte</p>
+          <p className='text-lg font-mono'>Deloitte</p>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,23 +1,22 @@
-import Image from "next/image";
+// import Image from "next/image";
 import JobCard from "./components/JobCard";
-
-
 
 export default function Home() {
   return (
-    <><div className='flex flex-col'>
-      <h1>Harry Song</h1>
-      <h2>Full Stack Developer</h2>
-      <p>Focused on creating performant web experiences.</p>
-    </div>
-    <div>
-      <h1>Work Experience</h1>
-      <JobCard />
-    </div>
-    
-    <div>
-      <h1>Connect</h1>
-    </div>
+    <>
+      <div className='flex flex-col'>
+        <h1>Harry Song</h1>
+        <h2>Full Stack Developer</h2>
+        <p>Focused on creating performant web experiences.</p>
+      </div>
+      <div>
+        <h1>Work Experience</h1>
+        <JobCard />
+      </div>
+
+      <div>
+        <h1>Connect</h1>
+      </div>
     </>
   );
 }
