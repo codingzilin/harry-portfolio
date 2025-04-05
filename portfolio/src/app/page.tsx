58734cfda9ad9220c675dbb74d554848ba77b02b
Zilin Song <harry.song518@gmail.com>
2025-04-05 14:22:15 +1030

    add padding for elements and setup font style

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,20 +3,22 @@ import JobCard from "./components/JobCard";
 
 export default function Home() {
   return (
-    <>
+    <div className='px-56 py-28'>
       <div className='flex flex-col'>
-        <h1>Harry Song</h1>
-        <h2>Full Stack Developer</h2>
-        <p>Focused on creating performant web experiences.</p>
+        <h1 className='font-mono text-3xl font-bold'>Harry Song</h1>
+        <h2 className='font-mono text-2xl'>Full Stack Developer</h2>
+        <p className='font-mono text-lg pt-10'>
+          Focused on creating performant web experiences.
+        </p>
       </div>
-      <div>
-        <h1>Work Experience</h1>
+      <div className='py-8'>
+        <h1 className='font-mono text-2xl font-bold pb-6'>Work Experience</h1>
         <JobCard />
       </div>
 
       <div>
-        <h1>Connect</h1>
+        <h1 className='font-mono text-2xl font-bold'>Connect</h1>
       </div>
-    </>
+    </div>
   );
 }
