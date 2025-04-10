cae5c7e5878435b501ea2262757ca4b6b9fb7eb8
Zilin Song <harry.song518@gmail.com>
2025-04-10 15:23:42 +0930

    feat: add app sidebar

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
+import { AppSidebar } from "./components/AppSidebar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,15 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SidebarProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SidebarProvider>
+          <AppSidebar />
           <SidebarTrigger />
           {children}
-        </body>
-      </html>
-    </SidebarProvider>
+        </SidebarProvider>
+      </body>
+    </html>
   );
 }
