cda6ef58a85741fb2b03888f8383537a7df786bd
Zilin Song <harry.song518@gmail.com>
2025-04-28 20:08:28 +0930

    feat-frontend-fix job title

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className='w-[65%] md:w-[90%] lg:w-[65%] max-w-7xl mx-auto py-10 md:py-20 lg:py-28'>
       <div className='flex flex-col'>
         <h1 className='font-mono text-2xl md:text-3xl font-bold'>Harry Song</h1>
-        <h2 className='font-mono text-xl md:text-2xl'>Full Stack Developer</h2>
+        <h2 className='font-mono text-xl md:text-2xl'>Full-Stack Developer</h2>
         <p className='font-mono text-base md:text-lg pt-6 md:pt-10'>
           Focused on creating performant web experiences.
         </p>
