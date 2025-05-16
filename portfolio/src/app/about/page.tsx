abfed2629e5e7aa1336681a3f38c74a2e9f5372e
Zilin Song <harry.song518@gmail.com>
2025-05-16 17:39:42 +0930

    add code editor component

--- portfolio/src/app/about/page.tsx
+++ portfolio/src/app/about/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { SkillSlider } from "../components/SkillsSlider";
+import IntroCodeEditor from "../components/CodeEditor";
 export default function page() {
   return (
     <div>
       <div>
         <h1>About me</h1>
-        <p>
+        <IntroCodeEditor />
+        {/* <p>
           I am a software engineer with a passion for building scalable and
           efficient web applications. I mainly focus on full stack development,
           but I am also interested in mobile development.
-        </p>
+        </p> */}
       </div>
       <div>
         <h1>Skills</h1>
---
+++ portfolio/src/app/components/CodeEditor.tsx
@@ -0,0 +1,30 @@
+import { CodeEditor } from "@/components/animate-ui/components/code-editor";
+
+export default function IntroCodeEditor() {
+  return (
+    <CodeEditor
+      lang='tsx'
+      cursor={true}
+      title='Introduction.tsx'
+      writing={false}
+      className='w-[640px] h-[480px]'
+      duration={15}
+      delay={0.5}
+    >
+      {`'use client';
+
+import React from 'react';
+
+const Introduction = () => {
+  return {
+    name: 'Harry Song',
+    role: 'Full Stack Developer',
+    location: 'Sydney/Adelaide, Australia',
+    expertise: 'Full Stack Development, Mobile Development, DevOps, SAP'
+  };
+};
+
+export default Introduction;`}
+    </CodeEditor>
+  );
+}
